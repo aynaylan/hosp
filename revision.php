ff531584a2f176e28584f2c97af3b41d627e49ab
Your Name <frankiearmstong@gmail.com>
2018-02-23 15:18:43 +0300

    first commit

---
+++ revision.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="container">
+		<nav class="nav navbar-inverse">
+			<ul class="navbar-nav">
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+			</ul>
+		</nav>
+
+		<form class="form-group">
+			Username<input type="text" class="form-input" name="">
+			Email<input type="email" class="form-input" name="">
+			Password<input type="password" class="form-input" name="">
+			Confirm-Password<input type="password" class="form-input" name="">
+		</form>
+	</div>
+
+</body>
+</html>
